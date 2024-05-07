e7a33e589937db8c916f25663e41de91835225fc
gabriel <vnvsgabe@gmail.com>
2024-05-07 16:23:44 -0300

    ajustado Input

---
+++ src/components/inputs/Input.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+const StyledInput = styled.input`
+  width: ${(props) => props.width || '100%'};
+  height: 45px;
+  background-color: transparent;
+  border: 2px solid #c1e3e3;
+  border-radius: 5px;
+  padding: 10px;
+  font-size: 14px;
+
+  outline: none;
+
+  &::placeholder {
+    color: #999;
+  }
+`
+
+const Input = ({ placeholder, width, type }) => {
+  return <StyledInput placeholder={placeholder} width={width} type={type} />
+}
+
+export default Input
