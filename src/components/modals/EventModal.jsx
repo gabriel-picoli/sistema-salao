bf97d79afc94ff150fd5182d49a7ed52ac51aae0
gabriel <vnvsgabe@gmail.com>
2024-05-10 19:44:04 -0300

    ajustado e adicionado props

--- src/components/modals/EventModal.jsx
+++ src/components/modals/EventModal.jsx
@@ -20,10 +20,11 @@ const ModalContainer = styled.div`
   left: 50%;
   transform: translate(-50%, -50%);
   background-color: ${(props) => props.theme.colors.white};
-  width: ${(props) => props.width || '950px'};
-  height: ${(props) => props.height || '300px'};
+  width: ${(props) => props.width || '1090px'};
+  min-height: ${(props) => props.minHeight || '300px'};
+  overflow: auto;
   border-radius: 5px;
-  padding: 15px 20px;
+  padding: 20px 30px;
 `
 const HeaderContainer = styled.div`
   display: flex;
@@ -47,11 +48,11 @@ const CloseButton = styled.button`
   }
 `
 
-export default function EventModal({ isOpen, onClose, children, width, height, title }) {
+export default function EventModal({ isOpen, onClose, children, width, minHeight, title }) {
   if (isOpen) {
     return (
       <ModalBackground>
-        <ModalContainer width={width} height={height}>
+        <ModalContainer width={width} minHeight={minHeight}>
           <HeaderContainer>
             <H3>{title}</H3>
             <CloseButton onClick={onClose}>
