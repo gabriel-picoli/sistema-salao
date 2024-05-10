fcd730238e8fa7c8a9db5f9d4b93d3ca8e05915b
gabriel <vnvsgabe@gmail.com>
2024-05-10 19:46:26 -0300

    contruçao e testes do modal no index

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-
 import styled from 'styled-components'
-
 import { FaCalendarAlt } from 'react-icons/fa'
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined'
+import AddRoundedIcon from '@mui/icons-material/AddRounded'
 
 import EventModal from '../src/components/modals/EventModal'
 import Input from '../src/components/inputs/Input'
 import Button from '../src/components/inputs/Button'
 import CalendarModal from '../src/components/modals/CalendarModal'
+import Dropdown from '../src/components/inputs/Dropdown'
 
 const ContentContainer = styled.div`
   display: flex;
@@ -39,15 +40,82 @@ const ClickCalendar = styled.div`
 const SeparatorLine = styled.div`
   height: 0.5;
   border: 0.5px solid #4e4e4e;
+  margin-bottom: 20px;
+`
+
+const InfoServicesContainer = styled.div`
+  width: 100%;
+  display: flex;
+  color: ${(props) => props.theme.colors.black};
+`
+
+const InfoServicesInputsContainer = styled.div`
+  width: 100%;
+  display: ${(props) => (props.visible ? 'flex' : 'none')};
+  align-items: center;
+  margin-bottom: 10px;
+`
+const CloseButton = styled.button`
+  background-color: transparent;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  font-size: 20px;
+  font-weight: bold;
+  color: ${(props) => props.theme.colors.primary};
+  display: flex;
+  align-items: flex-end;
+
+  :hover {
+    color: ${(props) => props.theme.colors.hoverPrimary};
+  }
+`
+
+const AddServiceContainer = styled.div`
+  width: 100%;
+  padding: 5px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: transparent;
+  color: ${(props) => props.theme.colors.primary};
+  cursor: pointer;
+  border: none;
+  outline: none;
+  margin-right: 20px;
+  margin-top: 20px;
+  border-radius: 10px;
+  transition: all 0.25s ease-in-out;
+
+  :hover {
+    color: ${(props) => props.theme.colors.hoverPrimary};
+    background-color: #c9c9c96a;
+  }
+`
+
+const AddText = styled.p`
+  color: ${(props) => props.theme.colors.black};
 `
 
 export default function HomePage() {
   const [openModal, setOpenModal] = useState(false)
   const [openCalendarModal, setOpenCalendarModal] = useState(false)
+  const [servicesVisible, setServicesVisible] = useState([true]) // Inicializa com um item visível
   const [hovered, setHovered] = useState(false)
 
+  const services = ['mechas', 'unhas', 'spa', 'mega hair']
+  const employees = ['pami', 'maria', 'juli', 'ange']
+
+  const handleCloseServicesContainer = (index) => {
+    setServicesVisible((prev) => prev.filter((_, i) => i !== index))
+  }
+
+  const handleAddServicesContainer = () => {
+    setServicesVisible((prev) => [...prev, true])
+  }
+
   return (
-    <div>
+    <>
       <button onClick={() => setOpenModal(true)}>abrir o modal</button>
       <EventModal isOpen={openModal} onClose={() => setOpenModal(false)} title="Agendamento">
         <ContentContainer>
@@ -63,7 +131,7 @@ export default function HomePage() {
               <ClickCalendar
                 onMouseEnter={() => setHovered(true)}
                 onMouseLeave={() => setHovered(false)}
-                onClick={() => setOpenCalendarModal(true)} // Alterando para abrir/fechar o modal
+                onClick={() => setOpenCalendarModal(true)}
               >
                 <FaCalendarAlt
                   size={30}
@@ -76,8 +144,8 @@ export default function HomePage() {
               <CalendarModal
                 isOpen={openCalendarModal}
                 onClose={() => setOpenCalendarModal(false)}
-                left="18.5%"
-                bottom="-40%"
+                left="50%"
+                bottom="50%" // Ajuste a posição do modal do calendário
               />
             </DatePickerContainer>
             <Button
@@ -89,9 +157,34 @@ export default function HomePage() {
               WHATSAPP
             </Button>
           </SecondContentContainer>
+          <InfoServicesContainer>
+            <p style={{ marginRight: '170px' }}>Serviço</p>
+            <p style={{ marginRight: '135px' }}>Profissional</p>
+            <p style={{ marginRight: '60px' }}>Tempo</p>
+            <p style={{ marginRight: '85px' }}>Início</p>
+            <p style={{ marginRight: '97px' }}>Fim</p>
+            <p>Valor</p>
+          </InfoServicesContainer>
           <SeparatorLine />
+          {servicesVisible.map((visible, index) => (
+            <InfoServicesInputsContainer key={index} visible={visible}>
+              <Dropdown options={services} text="Escolha um serviço" marginRight="26px" />
+              <Dropdown options={employees} text="Escolha um profissional" marginRight="26px" />
+              <Input width="90px" placeholder="Em mins" marginRight="26px" />
+              <Input width="100px" placeholder="07:30" marginRight="26px" />
+              <Input width="100px" placeholder="08:30" marginRight="26px" />
+              <Input width="150px" placeholder="R$ 180,00" marginRight="20px" />
+              <CloseButton onClick={() => handleCloseServicesContainer(index)}>
+                <CloseOutlinedIcon style={{ width: 23, height: 23 }} />
+              </CloseButton>
+            </InfoServicesInputsContainer>
+          ))}
+          <AddServiceContainer onClick={handleAddServicesContainer}>
+            <AddRoundedIcon style={{ width: 30, height: 30 }} />
+            <AddText>Adicionar Serviço</AddText>
+          </AddServiceContainer>
         </ContentContainer>
       </EventModal>
-    </div>
+    </>
   )
 }
