a517d10a1ffc8c6d14351cc0e9ef0beeb076c47f
gabriel <vnvsgabe@gmail.com>
2024-05-11 15:50:13 -0300

    teste de abertura do event modal

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,21 @@
+import { useState } from 'react'
 import EventModal from '../src/components/modals/EventModal'
+
 export default function HomePage() {
-  return <EventModal />
+  const [openModal, setOpenModal] = useState(false)
+
+  // Função para fechar o modal
+  const handleCloseModal = () => {
+    setOpenModal(false)
+  }
+
+  return (
+    <div>
+      {/* Botão para abrir o modal */}
+      <button onClick={() => setOpenModal(true)}>Abrir o modal</button>
+
+      {/* Renderização condicional do modal */}
+      {openModal && <EventModal isOpen={openModal} onClose={handleCloseModal} />}
+    </div>
+  )
 }
