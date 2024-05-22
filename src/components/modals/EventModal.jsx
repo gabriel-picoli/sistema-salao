41e8186e0b30810669eefc3b1689a48422e2e5fc
gabriel <vnvsgabe@gmail.com>
2024-05-22 15:48:47 -0300

    implementado useForm para consolar os dados de inputs

--- src/components/modals/EventModal.jsx
+++ src/components/modals/EventModal.jsx
@@ -1,11 +1,10 @@
+/* eslint-disable no-unused-vars */
 import { useState } from 'react'
-
+import { useForm } from 'react-hook-form'
 import styled from 'styled-components'
-
 import { FaCalendarAlt } from 'react-icons/fa'
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined'
 import AddRoundedIcon from '@mui/icons-material/AddRounded'
-
 import Modal from './Modal'
 import Input from '../inputs/Input'
 import Button from '../inputs/Button'
@@ -23,36 +22,30 @@ const FirstContentContainer = styled.div`
   justify-content: space-between;
   margin-bottom: 20px;
 `
-
 const SecondContentContainer = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 20px;
 `
-
 const DatePickerContainer = styled.div`
   display: flex;
   align-items: center;
   gap: 5px;
 `
-
 const ClickCalendar = styled.div`
   cursor: pointer;
 `
-
 const SeparatorLine = styled.div`
   height: 0.5;
   border: 0.5px solid #4e4e4e;
   margin-bottom: 20px;
   margin-top: 5px;
 `
-
 const InfoServicesContainer = styled.div`
   width: 100%;
   display: flex;
   color: ${(props) => props.theme.colors.black};
 `
-
 const InfoServicesInputsContainer = styled.div`
   width: 100%;
   display: ${(props) => (props.visible ? 'flex' : 'none')};
@@ -69,12 +62,10 @@ const CloseButton = styled.button`
   color: ${(props) => props.theme.colors.primary};
   display: flex;
   align-items: flex-end;
-
   :hover {
     color: ${(props) => props.theme.colors.hoverPrimary};
   }
 `
-
 const AddServiceContainer = styled.div`
   width: 100%;
   padding: 5px 0;
@@ -91,24 +82,20 @@ const AddServiceContainer = styled.div`
   margin-bottom: 20px;
   border-radius: 10px;
   transition: all 0.25s ease-in-out;
-
   :hover {
     color: ${(props) => props.theme.colors.hoverPrimary};
     background-color: #c9c9c96a;
   }
 `
-
 const AddText = styled.p`
   color: ${(props) => props.theme.colors.black};
 `
-
 const ClientStatusButtonContainer = styled.div`
   display: flex;
   align-items: center;
   margin-top: 30px;
   margin-bottom: 40px;
 `
-
 const FooterButtonsContainer = styled.div`
   display: flex;
   align-items: center;
@@ -118,11 +105,23 @@ const FooterButtonsContainer = styled.div`
 
 export default function EventModal({ isOpen, onClose }) {
   const [openCalendarModal, setOpenCalendarModal] = useState(false)
-  const [servicesVisible, setServicesVisible] = useState([true]) // inicializa com um item visível
-  const [hovered, setHovered] = useState(false)
+  const [servicesVisible, setServicesVisible] = useState([true])
+  const [selectedServices, setSelectedServices] = useState(Array(servicesVisible.length).fill(null))
 
   const services = ['mechas', 'unhas', 'spa', 'mega hair']
   const employees = ['pami', 'maria', 'juli', 'ange']
+
+  const { handleSubmit, control, reset } = useForm()
+
+  const onSubmit = (data) => {
+    console.log(data)
+    data.services.forEach((service, index) => {
+      console.log(`serviço ${index + 1}:`, service)
+    })
+    // fechar o modal e resetar o formulário após a submissão
+    // onClose()
+    // reset()
+  }
 
   const handleCloseServicesContainer = (index) => {
     setServicesVisible((prev) => prev.filter((_, i) => i !== index))
@@ -134,37 +133,33 @@ export default function EventModal({ isOpen, onClose }) {
 
   if (isOpen) {
     return (
-      <>
-        <Modal isOpen={isOpen} onClose={onClose} title="Agendamento">
+      <Modal isOpen={isOpen} onClose={onClose} title="Agendamento">
+        <form onSubmit={handleSubmit(onSubmit)}>
           <ContentContainer>
             <FirstContentContainer>
-              <Input placeholder="Cliente" width="650px" />
+              <Input name="client" control={control} placeholder="Cliente" width="650px" />
               <Button textWidth="100%" width="200px" height="42px">
                 CADASTRAR NOVO
               </Button>
             </FirstContentContainer>
             <SecondContentContainer>
               <DatePickerContainer>
-                <Input width="115px" placeholder="DD/MM/AAAA" />
-                <ClickCalendar
-                  onMouseEnter={() => setHovered(true)}
-                  onMouseLeave={() => setHovered(false)}
-                  onClick={() => setOpenCalendarModal(true)}
-                >
-                  <FaCalendarAlt
-                    size={30}
-                    style={{
-                      color: hovered ? '#4d9291c5' : '#64BAB8',
-                      transition: 'color 0.2s ease-in-out'
-                    }}
-                  />
+                <Input
+                  name="date"
+                  control={control}
+                  width="130px"
+                  placeholder="DD/MM/AAAA"
+                  type="date"
+                />
+                {/* <ClickCalendar onClick={() => setOpenCalendarModal(true)}>
+                  <FaCalendarAlt size={30} style={{ color: '#64BAB8' }} />
                 </ClickCalendar>
                 <CalendarModal
                   isOpen={openCalendarModal}
                   onClose={() => setOpenCalendarModal(false)}
                   left="15.5%"
                   bottom="20%"
-                />
+                /> */}
               </DatePickerContainer>
               <Button textWidth="100%" width="130px" height="42px">
                 WHATSAPP
@@ -181,12 +176,56 @@ export default function EventModal({ isOpen, onClose }) {
             <SeparatorLine />
             {servicesVisible.map((visible, index) => (
               <InfoServicesInputsContainer key={index} visible={visible}>
-                <Dropdown options={services} text="Escolha um serviço" marginRight="40px" />
-                <Dropdown options={employees} text="Escolha um profissional" marginRight="40px" />
-                <Input width="90px" placeholder="Em mins" marginRight="40px" />
-                <Input width="100px" placeholder="07:30" marginRight="40px" />
-                <Input width="100px" placeholder="08:30" marginRight="40px" />
-                <Input width="150px" placeholder="R$ " marginRight="40px" isMoneyInput />
+                <Dropdown
+                  options={services}
+                  text="Escolha um serviço"
+                  marginRight="40px"
+                  onChange={(selectedOption) => {
+                    const updatedServices = [...selectedServices]
+                    updatedServices[index] = selectedOption
+                    setSelectedServices(updatedServices)
+                  }}
+                />
+                <Dropdown
+                  options={employees}
+                  text="Escolha um funcionário"
+                  marginRight="40px"
+                  onChange={(selectedOption) => {
+                    const updatedServices = [...selectedServices]
+                    updatedServices[index] = selectedOption
+                    setSelectedServices(updatedServices)
+                  }}
+                />
+                <Input
+                  name={`services[${index}].time`}
+                  control={control}
+                  width="90px"
+                  placeholder="Em mins"
+                  marginRight="40px"
+                />
+                <Input
+                  name={`services[${index}].start`}
+                  control={control}
+                  width="100px"
+                  placeholder="07:30"
+                  marginRight="40px"
+                />
+                <Input
+                  name={`services[${index}].end`}
+                  control={control}
+                  width="100px"
+                  placeholder="08:30"
+                  marginRight="40px"
+                />
+                <Input
+                  name={`services[${index}].value`}
+                  control={control}
+                  width="150px"
+                  placeholder="R$"
+                  marginRight="40px"
+                  isMoneyInput
+                />
+
                 <CloseButton onClick={() => handleCloseServicesContainer(index)}>
                   <CloseOutlinedIcon style={{ width: 23, height: 23 }} />
                 </CloseButton>
@@ -197,7 +236,7 @@ export default function EventModal({ isOpen, onClose }) {
               <AddText>Adicionar Serviço</AddText>
             </AddServiceContainer>
             <p>Total a pagar</p>
-            <Input isMoneyInput placeholder="R$" />
+            <Input name="total" control={control} isMoneyInput placeholder="R$" />
             <ClientStatusButtonContainer>
               <ClientStatusButton status="confirmed">Confirmado</ClientStatusButton>
               <ClientStatusButton status="scheduled">Agendado</ClientStatusButton>
@@ -214,6 +253,7 @@ export default function EventModal({ isOpen, onClose }) {
               backgroundColor="#eee"
               textColor="black"
               hoverBackgroundColor="#c0c0c07f"
+              onClick={onClose}
             >
               Cancelar
             </Button>
@@ -221,8 +261,10 @@ export default function EventModal({ isOpen, onClose }) {
               Confirmar
             </Button>
           </FooterButtonsContainer>
-        </Modal>
-      </>
+        </form>
+      </Modal>
     )
   }
+
+  return null
 }
